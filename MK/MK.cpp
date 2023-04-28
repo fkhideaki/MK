72ffbe0e8409b16ab4b244b81808870cf20ee8e0
fkhd <fkhideaki@gmail.com>
2023-04-29 04:16:26 +0900

    オプション追加 /tt 今日の日付.txt　のファイルを作成 /td 今日の日付のフォルダを作成 /t 今日の日付のファイルを作成

--- MK/MK.cpp
+++ MK/MK.cpp
@@ -1,8 +1,12 @@
-﻿#include "pch.h"
+﻿
+#include "pch.h"
 #include <stdio.h>
 #include <windows.h>
 #include <locale.h>
 #include <iostream>
+#include <time.h>
+#include <direct.h>
+#include <stdlib.h>
 using namespace std;
 
 enum class ExistsState
@@ -12,7 +16,7 @@ enum class ExistsState
 	Nothing,
 };
 
-ExistsState getExistsState(wchar_t* objPath)
+ExistsState getExistsState(const wchar_t* objPath)
 {
 	WIN32_FIND_DATA data;
 	HANDLE hFile = FindFirstFile(objPath, &data);
@@ -27,7 +31,7 @@ ExistsState getExistsState(wchar_t* objPath)
 	return ExistsState::File;
 }
 
-bool makeFileMain(wchar_t* fn)
+bool makeFileIfNotExists(const wchar_t* fn)
 {
 	ExistsState exs = getExistsState(fn);
 	if (exs != ExistsState::Nothing) {
@@ -46,6 +50,53 @@ bool makeFileMain(wchar_t* fn)
 	fclose(fp);
 
 	return true;
+}
+
+bool makeDirIfNotExists(const wchar_t* fn)
+{
+	ExistsState exs = getExistsState(fn);
+	if (exs != ExistsState::Nothing) {
+		wcout << fn << L" is exists" << endl;
+		return false;
+	}
+
+	return _wmkdir(fn) == 0;
+}
+
+std::wstring getToday()
+{
+	wchar_t s[100];
+
+	time_t t = time(NULL);
+	struct tm* tp = localtime(&t);
+	size_t rc = wcsftime(s, sizeof(s) / 2, L"%Y%m%d", tp);
+	return s;
+}
+
+bool makeFileMain(wchar_t* fn)
+{
+	std::wstring s = fn;
+	if (s.length() >= 3)
+	{
+		if (s.substr(0, 3) == L"/tt")
+		{
+			std::wstring today = getToday();
+			today += L".txt";
+			return makeFileIfNotExists(today.c_str());
+		}
+		else if (s.substr(0, 3) == L"/td")
+		{
+			std::wstring today = getToday();
+			return makeDirIfNotExists(today.c_str());
+		}
+		else if (s.substr(0, 2) == L"/t")
+		{
+			std::wstring today = getToday();
+			today += s.substr(2);
+			return makeFileIfNotExists(today.c_str());
+		}
+	}
+	return makeFileIfNotExists(fn);
 }
 
 int wmain(int argc, wchar_t* argv[])
